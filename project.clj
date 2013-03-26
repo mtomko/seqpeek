94207744122ce877403a08a889d5fdd408dd8198
mtomko <mjt0229@gmail.com>
2013-03-25 21:11:27 -0400

    added count-reads command

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/mtomko/seqpeek"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[incanter "1.4.1"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.biojava/biojava3-core "3.0.5"]
-                 [org.biojava/biojava3-sequencing "3.0.5"]
-                 ]
+                 [org.biojava/biojava3-sequencing "3.0.5"]]
   :repositories [["biojava" "http://biojava.org/download/maven/"]]
   :main seqpeek.core)
