9d5ad534e2084fc44cc4a0cac10a63397569ea14
mtomko <mjt0229@gmail.com>
2013-03-25 21:11:27 -0400

    added count-reads command

--- src/seqpeek/core.clj
+++ src/seqpeek/core.clj
@@ -1,23 +1,27 @@
 (ns seqpeek.core
-  (:import (org.biojava3.sequencing.io.fastq FastqReader IlluminaFastqReader)
-           (java.io File))
-  (:gen-class))
+  (:import [org.biojava3.sequencing.io.fastq FastqReader IlluminaFastqReader]
+           [java.io File])
+  (:gen-class)) 
 
 (defmulti count-reads
   "Returns the number of reads in a sequencing data file"
   class)
 
 (defmethod count-reads File [file]
-  (let [reader (IlluminaFastqReader. file)]
-    (count (.read reader))))
+  (let [reader (IlluminaFastqReader.)]
+    (count (seq (.read reader file)))))
 
 (defmethod count-reads String [filename]
   (count-reads (File. filename)))
 
-
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+  "The main entry point for seqpeek"
+  [command & args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+
+  ;; delegate base on command passed by the user
+  (case command
+    "count-reads" (println (count-reads (first args)))
+    (println (str "Unknown command: " (first args)))))
+
