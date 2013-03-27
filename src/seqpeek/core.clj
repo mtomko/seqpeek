47b67bc92439315b637706d4ba6f8ac9fc7e482e
mtomko <mjt0229@gmail.com>
2013-03-26 20:20:01 -0400

    missed in previous commit

--- src/seqpeek/core.clj
+++ src/seqpeek/core.clj
@@ -1,35 +1,25 @@
 (ns seqpeek.core
-  ;; (:require [clojure.core.reducuers :as r])
+  (:require [seqpeek.count])
+  (:use [seqpeek.seq])
   (:import java.io.File
-           (org.biojava3.sequencing.io.fastq FastqReader IlluminaFastqReader SangerFastqReader))
+           (org.biojava3.sequencing.io.fastq FastqReader
+                                             IlluminaFastqReader
+                                             SangerFastqReader
+                                             SolexaFastqReader))
   (:gen-class)) 
 
-(defn fastq-seq
-  "Returns a sequence view of the FASTQ reader."
-  [reader file]
-  (seq (.read reader file )))
-
-(defn sequences
-  "Returns a lazy sequence of just the raw sequence data in the FASTQ"
-  [reader file]
-  (map #(.getSequence %) (fastq-seq reader file)))
-
-(defn count-long-reads 
-  [reader file n]
-  (count (filter
-          #(<= n (count %))
-         (sequences reader file))))
-
+(declare main)
 (defn -main
   "The main entry point for seqpeek"
-  [command & args]
+  [command dialect & args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
   ;; delegate base on command passed by the user
   (case command
-    "count-reads" (println
-                   (count (fastq-seq (IlluminaFastqReader.)
+    "count" (seekpeek.count/count-cmd dialect args)
+    "count-reads" (println 
+                   (count (fastq-seq (get-reader dialect)
                                      (File. (first args)))))
     "count-long-reads" (println
                         (count-long-reads (IlluminaFastqReader.)
