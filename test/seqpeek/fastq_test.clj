92dfa90602c0eaf199d0653d8bbbf88f8c36d210
mtomko <mjt0229@gmail.com>
2013-03-27 22:27:39 -0400

    Improve fastq-seq
    Use for comprehension to select relevant lines and construct a map from
    the resulting vector.

--- test/seqpeek/fastq_test.clj
+++ test/seqpeek/fastq_test.clj
@@ -11,5 +11,10 @@
 (deftest test-fastq-sequences
   (testing "seqpeek.fastq/fastq-sequences"
     (is (= (seq (list "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGATACGCTGCTTTA"))
-           (fastq-sequences fastq-record1)))))
+           (fastq-sequences fastq-record1))))
+  (testing "seqpeek.fastq/fastq-seq")
+    (is (= (seq (list {:id "@JE07D:00028:00073"
+                       :seq "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGATACGCTGCTTTA"
+                       :qual ";>>BABCD@CCCB;?49919@9?@ABA>BB;<=BBB>B;;8<AA@>@@999@19"}))
+           (fastq-seq fastq-record1))))
            
