6c84f6b8b5ce65bfd7d5b5aefd0b8f9759320331
Mark Tomko <mtomko@broadinstitute.org>
2013-03-29 17:51:53 -0400

    progress on making the tool a real program

--- src/seqpeek/core.clj
+++ src/seqpeek/core.clj
@@ -1,26 +1,16 @@
 (ns seqpeek.core
-  (:use seqpeek.count-reads
-        seqpeek.fastq
-        seqpeek.file
-        seqpeek.seq)
+  (:use [clojure.string :only [lower-case]]
+        seqpeek.count-reads)
   (:gen-class)) 
-
-(defn sequences
-  [filename]
-  (-> filename make-line-seq fastq-sequences))
 
 (defn -main
   "The main entry point for seqpeek"
-  [command dialect & args]
+  [command & args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (case (clojure.string/lower-case command)
-    "count" (count-reads dialect args)
-    "count-reads" (println 
-                   (count (sequences (first args))))
 
-    "count-long-reads" (println
-                        (count-long-reads (sequences (first args))
-                                          (Integer/parseInt (second args))))
-    (println (str "Unknown command: " (first args)))))
+  ;; decipher the command and dispatch to the appropriate authority
+  (case (lower-case command)
+    "count" (count-reads args)
+    (println (str "Unknown command: " command))))
 
--- src/seqpeek/count_reads.clj
+++ src/seqpeek/count_reads.clj
@@ -1,5 +1,7 @@
 (ns seqpeek.count-reads
-  (:require [seqpeek.seq])
+  (:require seqpeek.fastq
+            seqpeek.file
+            seqpeek.seq)
   (:use [clojure.tools.cli :only[cli]]))
 
 (defn- parse-args
@@ -10,14 +12,9 @@
        ["-m" "--max-length" "Filter reads by maximum length" :parse-fn #(Integer. %)]
        ["-h" "--help" "Display usage and quit" :flag true]))
 
-(defn count-reads
-  "The entry point for the count-reads command."
-  [dialect args]
-  (let [[options files banner] (parse-args args)]
-    (when (:help options)
-      (println banner)
-      (System/exit 0))
-    (println options)))
+(defn- fastq-seq-over
+  [filename]
+  (-> filename seqpeek.file/make-line-seq seqpeek.fastq/fastq-seq))
 
 (defn- build-filter
   "Creates a filter predicate based on the provided options."
@@ -27,3 +24,23 @@
           (nil? lb) (fn [x] (< x ub))
           (nil? ub) (fn [x] (>= x lb))
           :else (fn [x] (and (< x ub) (>= x lb))))))
+
+(defn- count-reads-for-file
+  "Counts reads in the input file matching the provided filter."
+  [filename seqfilter]
+  (let [fseq (fastq-seq-over filename)
+        sequences (map :seq fseq)]
+    (count (filter seqfilter sequences))))
+
+(defn count-reads
+  "The entry point for the count-reads command."
+  [args]
+  (let [[options files banner] (parse-args args)]
+    (when (:help options)
+      (println banner)
+      (System/exit 0))
+    (let [seqfilter (build-filter options)
+          print-filenames (> 1 (count files))]
+      (doseq [filename files]
+        (when print-filenames println (str filename ": "))
+        (println  (str "\t" (count-reads-for-file filename seqfilter)))))))
