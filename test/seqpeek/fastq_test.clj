7cca4389b16a6a66d18ed774f7d92517bd520525
Mark Tomko <mtomko@broadinstitute.org>
2013-04-02 18:58:02 -0400

    add filter tests; some code cleanup

--- test/seqpeek/fastq_test.clj
+++ test/seqpeek/fastq_test.clj
@@ -8,6 +8,21 @@
    "+"
    ";>>BABCD@CCCB;?49919@9?@ABA>BB;<=BBB>B;;8<AA@>@@999@19"])
 
+(def fastq-record2
+  ["@JE07D:00031:00075"
+   "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGAAAGGACGAACCGCCGAGTCTGC"
+   "+"
+   "@@6;8;BC?AB@;<999A<BA@B?<;;19C@<<=CCBCCC@BA>B@BBB;;;6.0,.....14/"])
+
+(def fastq-record3
+  ["@JE07D:00033:00077"
+   "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGAAAGGACGAGGTCCCACATCCTCACCCC"
+   "+"
+   ">@49544959BBBCCCCCBCC@C@BCD@DDBBBCCCACCB=@><@<@@>9958991/,,,,*,,,0000"])
+
+(def fastq-sequence
+  (seq (list fastq-record1 fastq-record2 fastq-record3)))
+
 (deftest test-fastq-sequences
   (testing "seqpeek.fastq/fastq-sequences"
     (is (= (seq (list "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGATACGCTGCTTTA"))
@@ -17,4 +32,3 @@
                        :seq "TAAGGAGAACGATCGATTTCTTGGCTTTATATATCTTGTGGATACGCTGCTTTA"
                        :qual ";>>BABCD@CCCB;?49919@9?@ABA>BB;<=BBB>B;;8<AA@>@@999@19"}))
            (fastq-seq fastq-record1))))
-           
