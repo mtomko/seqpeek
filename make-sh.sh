af92fab39d040e33602d16b5a121a26dd22d0f42
mtomko <mjt0229@gmail.com>
2013-04-13 11:33:52 -0400

    Sample command functional.

--- make-sh.sh
+++ make-sh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # stolen from http://github.com/daveray/clawk
-(echo '#!/usr/bin/env java -jar'; cat target/*-standalone.jar) > target/seqpeek
+(echo '#!/usr/bin/env java -XX:-UseGCOverheadLimit -jar'; cat target/*-standalone.jar) > target/seqpeek
 chmod u+x target/seqpeek
