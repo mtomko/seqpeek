fc0e00f68366ebbb1081df95d9781d277a511a64
mtomko <mjt0229@gmail.com>
2013-06-05 22:08:49 -0400

    Change test frameworks to speclj

--- project.clj
+++ project.clj
@@ -3,10 +3,13 @@
   :url "http://github.com/mtomko/seqpeek"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["biojava" "http://biojava.org/download/maven/"]]
   :dependencies [[incanter "1.4.1"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [org.biojava/biojava3-core "3.0.5"]
-                 [org.biojava/biojava3-sequencing "3.0.5"]]
-  :repositories [["biojava" "http://biojava.org/download/maven/"]]
+                 [org.clojure/tools.cli "0.2.2"]]
+                 ;[org.biojava/biojava3-core "3.0.5"]
+                 ;[org.biojava/biojava3-sequencing "3.0.5"]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :plugins [[speclj "2.5.0"]]
+  :test-paths ["spec/"]
   :main seqpeek.core)
